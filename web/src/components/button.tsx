b778b4748fc50b7fcb0ebf1c51edcb48b493bcef
Diego Viana <dsantos@inf.ufsm.br>
2025-02-19 17:23:12 -0300

    Update button.tsx

--- web/src/components/button.tsx
+++ web/src/components/button.tsx
@@ -1,14 +1,15 @@
-import { ReactNode } from 'react'
+import { ComponentProps, ReactNode } from 'react'
 
-interface ButtonProps {
+interface ButtonProps extends ComponentProps<'button'> {
   children: ReactNode
 }
 
-const Button = ({ children }: ButtonProps) => {
+const Button = (props: ButtonProps) => {
   return (
-    <button className="text-blue hover:bg-blue flex h-12 w-full cursor-pointer items-center justify-between rounded-xl bg-gray-500 px-5 font-semibold transition-colors duration-300 hover:text-gray-900">
-      {children}
-    </button>
+    <button
+      className="text-blue hover:bg-blue flex h-12 w-full cursor-pointer items-center justify-between rounded-xl bg-gray-500 px-5 font-semibold transition-colors duration-300 hover:text-gray-900"
+      {...props}
+    />
   )
 }
 
