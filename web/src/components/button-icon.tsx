ea87b552a70fe172bfa43246854e220fd2414e34
Diego Viana <dsantos@inf.ufsm.br>
2025-02-19 17:22:14 -0300

    Update button-icon.tsx

--- web/src/components/button-icon.tsx
+++ web/src/components/button-icon.tsx
@@ -1,14 +1,15 @@
-import { ReactNode } from 'react'
+import { ComponentProps, ReactNode } from 'react'
 
-interface IconButtonProps {
+interface IconButtonProps extends ComponentProps<'button'> {
   children: ReactNode
 }
 
-const ButtonIcon = ({ children }: IconButtonProps) => {
+const ButtonIcon = (props: IconButtonProps) => {
   return (
-    <button className="text-blue hover:bg-blue h-12 cursor-pointer rounded-md bg-gray-500 p-1.5 transition-colors duration-300 hover:text-gray-900">
-      {children}
-    </button>
+    <button
+      className="text-blue hover:bg-blue h-12 cursor-pointer rounded-md bg-gray-500 p-1.5 transition-colors duration-300 hover:text-gray-900"
+      {...props}
+    />
   )
 }
 
