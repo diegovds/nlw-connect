c4450cdfe15a2f5653898bfe6be535bd6db5f321
Diego Viana <dsantos@inf.ufsm.br>
2025-02-19 22:04:10 -0300

    Update button-icon.tsx

--- web/src/components/button-icon.tsx
+++ web/src/components/button-icon.tsx
@@ -1,13 +1,17 @@
 import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 /* eslint-disable */
 interface IconButtonProps extends ComponentProps<'button'> {}
 /* eslint-enable */
 
-const ButtonIcon = (props: IconButtonProps) => {
+const ButtonIcon = ({ className, ...props }: IconButtonProps) => {
   return (
     <button
-      className="text-blue hover:bg-blue h-12 cursor-pointer rounded-md bg-gray-500 p-1.5 transition-colors duration-300 hover:text-gray-900"
+      className={twMerge(
+        'text-blue hover:bg-blue cursor-pointer rounded-md bg-gray-500 p-1.5 transition-colors duration-300 hover:text-gray-900',
+        className,
+      )}
       {...props}
     />
   )
