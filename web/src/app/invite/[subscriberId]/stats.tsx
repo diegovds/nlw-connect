7929d56634079ffa699d541e3e2e60b600d2c4ca
Diego Viana <dsantos@inf.ufsm.br>
2025-02-25 20:15:20 -0300

    Update stats.tsx

--- web/src/app/invite/[subscriberId]/stats.tsx
+++ web/src/app/invite/[subscriberId]/stats.tsx
@@ -37,7 +37,7 @@ export async function Stats({ subscriberId }: StatsProps) {
       </div>
       <div className="relative flex flex-col items-center justify-center gap-1 rounded-xl border border-gray-600 bg-gray-700 px-4 py-7">
         <span className="font-heading text-2xl leading-none font-semibold text-gray-200">
-          {rankingPosition ? `${rankingPosition}&ordm;` : '-'}
+          {rankingPosition ? `${rankingPosition}º` : '-'}
         </span>
         <span className="text-center text-sm leading-none text-gray-300">
           Posição no ranking
