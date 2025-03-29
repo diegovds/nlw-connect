6e6ebba5ac59bf6f148777b28a5412bbb643d287
Diego Viana <dsantos@inf.ufsm.br>
2025-03-28 21:46:10 -0300

    init

---
+++ server/src/server.ts
@@ -0,0 +1,41 @@
+import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import fastify from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors, {
+  origin: true,
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'NLW Connect',
+      version: '0.0.1',
+      description: 'API para o NLW Connect',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(subscribeToEventRoute)
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('🚀 HTTP Server Running!')
+})
